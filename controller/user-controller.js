1a3abb6666008003823d475a86e2f1516253ab2a
PrabhneetSingh02 <prabhneet2002@gmail.com>
2024-01-18 14:48:05 +0530

    added login Api

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,5 +1,11 @@
-import User from '../model/user.js';
 import bcrypt from 'bcrypt';   //for encrypting the password in database. we send salt+ hashedPassword
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+import User from '../model/user.js';
+import Token from '../model/token.js';
+
+dotenv.config();
 
 export const signupUser = async (request, response) => {
     try{
@@ -8,15 +14,41 @@ export const signupUser = async (request, response) => {
         
         const user =  {username: request.body.username, name: request.body.name, password: hashedPassword }
 
-        const newUser = new User(user);     // validated object. we validate using User imported from user.js in which schema is defined.
-        try{
-            await newUser.save();
-        } catch(error){}
+        const newUser = await User.create(user);   // validated object. we validate using User imported from user.js in which schema is defined.
+        if(!newUser){
+            throw '500 internal server error';
+        }
 
         return response.status(200).json({ msg: 'signup successful' });
     } catch(error){
         return response.status(500).json({ msg: 'Error while signup the user'});
     }
 }
-
 // export default signupUser;    //written for learning purpose
+
+export const loginUser = async (request, response) => {
+    let user = await User.findOne({ username: request.body.username });
+    if(!user){
+        return response.status(400).json({ msg: 'Username doesnot match' });
+    }
+
+    try{
+        let match = await bcrypt.compare(request.body.password, user.password);
+        if(match){
+            // if match, then generate access token and refresh token 
+            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m' });
+            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+
+            const newToken = new Token({ token: refreshToken });
+            
+            try{ await newToken.save();}
+            catch(error){}
+            return response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken, name: user.name, username: user.username })
+
+        } else{
+            return response.status(400).json({ msg: 'Password doesnot match' });
+        }
+    } catch(error){
+        return response.status(500).json({ msg: 'Error while login'});
+    }
+}
